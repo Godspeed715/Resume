358a8a34bdc17ed94111c64967bfe4b0502ab88e
Godspeed715 <chimzagamdaniel@gmail.com>
2025-09-23 17:22:55 +0100

    modified App.jsx imported individual page areas

--- src/App.jsx
+++ src/App.jsx
@@ -1,35 +1,26 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+import { useState } from 'react';
+import './App.css';
+import MoreData from './page_areas/MoreData';
+import Header from './page_areas/header';
+import AboutMe from './page_areas/AboutMe';
+import Experience from './page_areas/Experience';
+import ProfilePicture from './page_areas/ProfilePicture';
 
 function App() {
-  const [count, setCount] = useState(0)
+	const [count, setCount] = useState(0);
+	const pageAreas = [
+		Header(),
+		ProfilePicture(),
+		AboutMe(),
+		Experience(),
+		MoreData(),
+	];
 
-  return (
-    <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
-  )
+	const display = pageAreas.map((e) => {
+		return e;
+	});
+
+	return <div className="container">{display}</div>;
 }
 
-export default App
+export default App;
